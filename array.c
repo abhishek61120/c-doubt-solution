cc03179523fa6b4ff657efe80e6aafb185f25c5f
okbajiya <99985753+okbajiya@users.noreply.github.com>
2022-04-26 19:21:04 +0530

    Traversing 1-d array

---
+++ array.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+
+int i, arr[50],n ;
+
+printf("\n Enter the size of array: ");
+scanf("%d" ,&n);
+
+printf("\n Enter the size of array: ");
+for ( i=0; i < n; i++)
+{
+    printf("\n Array Element: %d" , i+l);
+    scanf("%d" ,&arr[i]);
+}       
+printf("\n Array elements and their addresses are : \n")
+for(i=0; i<n; i++)
+
+{
+    printf("\n%d  %u" , arr[i], &arr[i]);
+
+}
+getch();
+
+}
